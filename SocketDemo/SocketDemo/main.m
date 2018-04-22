3d33dfed050136cfc9b0e910d1db9a3f07ceceb4
Dino <kill_death@163.com>
2018-04-22 13:01:46 +0800

    Demo初步搭建

---
+++ SocketDemo/SocketDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SocketDemo
+//
+//  Created by 魏欣宇 on 2018/4/22.
+//  Copyright © 2018年 Dino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
